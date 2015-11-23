5ea66df95f92e0f4484b81ac22bcd916912144ab
Shun Ito <ShunIto2015@users.noreply.github.com>
2015-11-23 21:49:59 +0900

    add tmux2.0 install script

--- install_script_for_Ubuntu14.04LTS.sh
+++ install_script_for_Ubuntu14.04LTS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get -y update && sudo apt-get -y upgrade
-sudo apt-get -y install build-essential libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev zsh git vim tmux ssh openssh-server openssh-client
+sudo apt-get -y install build-essential libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev zsh git vim tmux ssh openssh-server openssh-client libevent-dev
 cd ~ && git clone https://github.com/yyuu/pyenv.git ./.pyenv
 mkdir -p ./.pyenv/versions ./.pyenv/shims
 cd .pyenv/plugins/ && git clone https://github.com/yyuu/pyenv-virtualenv.git
---
+++ install_tmux_for_Ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -y tmux=2.0-1~ppa1~t
